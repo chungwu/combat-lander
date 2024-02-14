c5951f75ec2ad1e47a4cec2bd5137cf57a10e6c6
Chung Wu <chungwu@plasmic.app>
2024-02-13 17:01:05 -0800

    Use smaller ids for room ids

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import { useIsomorphicLayoutEffect } from "@/hooks/react-utils";
-import { nanoid } from "nanoid";
-import { Inter } from "next/font/google";
+import { customAlphabet } from "nanoid";
 import { useRouter } from "next/router";
+
+// We don't care that much about collision, so use a small,
+// easy to share alphabet
+const makeId = customAlphabet('23456789abcdefghijkmnpqrstuvwxyz', 6);
 
 export default function Home() {
   const router = useRouter();
   useIsomorphicLayoutEffect(() => {
-    router.push(`/${nanoid()}`);
+    router.push(`/${makeId()}`);
   }, [router]);
   return null;
 }
