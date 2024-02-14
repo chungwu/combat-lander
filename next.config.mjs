a22a0d2b302b623696b3e95d85566d3a7f1f6e3b
Chung Wu <chungwu@plasmic.app>
2024-02-13 16:42:59 -0800

    Fix nextjs build for dynamic wasm import
    https://github.com/vercel/next.js/issues/25852

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config) {
-    config.experiments = { asyncWebAssembly: true, layers: true }
+  webpack(config, { isServer }) {
+    config.experiments = { asyncWebAssembly: true };
+
+    // https://github.com/vercel/next.js/issues/25852
+    if (isServer) {
+      config.output.webassemblyModuleFilename = "./../static/wasm/[modulehash].wasm";
+    } else {
+      config.output.webassemblyModuleFilename = "static/wasm/[modulehash].wasm";
+    }
     return config;
   }
 };
