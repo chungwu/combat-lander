2ddc4f00d475a9bfb785b05e3edb992cd0b32364
Chung Wu <chungwu@plasmic.app>
2024-02-21 16:35:25 -0800

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
       `}</style>
       <Head>
         <title>Combat Lander</title>
+        <link rel="icon" href="lander.svg" />
       </Head>
       <div className={`app dark ${MONO.variable}`}>
         <Component {...pageProps} />
