7ca509e1ee6500aa4c20a24031e6460bae7b9335
Chung Wu <chungwu@plasmic.app>
2024-02-21 16:18:04 -0800

    Enable production source maps

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
   webpack(config, { isServer }) {
     config.experiments = { asyncWebAssembly: true };
 
